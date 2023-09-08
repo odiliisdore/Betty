36f808ea65d89c49f740c61f75db87d995c1883b
Onyeodili Onyekwere Isdore <onyeodiliisdore@gmail.com>
2023-09-07 20:18:51 -0700

    size

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'programming in c'
+ *
+ * Return: always 0 (suceess)
+ */
+int main(void)
+{
+	printf("\"with proper grammar, but the outcome is a piece of art");
+	return (0);
+}
