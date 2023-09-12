294b96b9295b7ec75e320689064fb9ed1cce190c
Onyeodili Onyekwere Isdore <onyeodiliisdore@gmail.com>
2023-09-12 09:38:38 -0700

    hello

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -9,7 +9,7 @@
 
 int main(void)
 {
-	printf(stderr,"\"with proper grammar, but the outcome is a piece of art,");
+	printf("\n"with proper grammar, but the outcome is a piece of art,");
 
 	return (0);
 }
