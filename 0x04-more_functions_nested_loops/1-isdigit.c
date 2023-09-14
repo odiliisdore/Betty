ffef735d5472a6547d676ba82a5d6dc04adfe9dc
Onyeodili Onyekwere Isdore <onyeodiliisdore@gmail.com>
2023-09-14 13:24:51 -0700

    push

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isdigit - Checks for a digit (0 through 9)
+ * @c: The digits to be checked
+ * Return: 1 if c is a digit or 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+        if (c >= 48 && c <= 57)
+        {
+                return (1);
+        }
+        return (0);
+}
+~                   
