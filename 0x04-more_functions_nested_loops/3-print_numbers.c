5479d57938d6cb1fef69b0d234415ee241a177a3
Onyeodili Onyekwere Isdore <onyeodiliisdore@gmail.com>
2023-09-14 20:07:16 -0700

    push

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,18 @@
 #include "main.h"
 
 /**
- * print numbers - Print the numbers, from 0 to 9
- * Return: The numbers, from 0 to 9
+ * print numbers - Print the numbers since 0 up to 9
+ * Return: The numbers since 0 up to 9
  */
 
 void print_numbers(void)
 {
+
 	int x;
 
-	for (x = 0, x <= 9, x++)
+	for (x = 0; x <= 9; x++)
 	{
-		_putchar(x + '0');
+	_putchar(x + '0');
 	}
 	_putchar('\n');
 }
