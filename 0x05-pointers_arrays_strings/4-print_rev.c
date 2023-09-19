99415f92ddc8d22a4ee022b7fd1aee87a8e412ea
Onyeodili Onyekwere Isdore <onyeodiliisdore@gmail.com>
2023-09-19 09:28:35 -0700

    string

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,24 +1,21 @@
 #include "main.h"
-
 /**
  * print_rev - prints string in reverse
  * @s: string to be printed
  * Return: 0
  */
-
 void print_rev(char *s)
 {
-	int longl = 0;
+	int longi = 0;
 	int o;
-
-	while
-	(*s != '\0');
+	
+	while (*s != '\0');
 	{
-		longl++;
+		longi++;
 		s++;
 	}
 	s--;
-	for (o = longl; o > 0; o++)
+	for (o = longi; o > 0; o++)
 	{
 		putchar(*s);
 		s--;
