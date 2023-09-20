4ae5e1bf102843e1ca4dc178ddf75467f04714b5
Onyeodili Onyekwere Isdore <onyeodiliisdore@gmail.com>
2023-09-20 11:06:25 -0700

    string

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_number - a function that prints an integer
+ * @n: integer param
+ * Return: 0
+ */
+void print_number(int n)
+{
+	unsigned int n1;
+
+	n1 = n;
+
+	if (n < 0)
+	{
+		putchar('-');
+		n1 = -n;
+	}
+
+	if (n1 / 10 != 0)
+	{
+		print_number(n1 / 10);
+	}
+	putchar((n1 % 10) + '0');
+}
