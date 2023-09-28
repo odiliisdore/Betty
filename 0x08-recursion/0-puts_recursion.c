bed5e85eb084fd25c1a866c878b7d9b461359cde
Onyeodili Onyekwere Isdore <onyeodiliisdore@gmail.com>
2023-09-28 15:08:58 -0700

    puts

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * puts_recursion - function that prints a string
+ * _puts_recursion - function that prints a string
  * @s: input
  * Return: Always 0 (Success)
  */
@@ -11,8 +11,8 @@ void _puts_recursion(char *s)
 	if (*s)
 	{
 		putchar(*s);
-		_put_recursion(s + 1);
+		_puts_recursion(s + 1);
 	}
 	else
-		_putchar('\n');
+		putchar('\n');
 }
