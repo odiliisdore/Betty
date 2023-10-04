89d5bf0d29543d928e52cbc979bceaf30ac758fa
Onyeodili Onyekwere Isdore <onyeodiliisdore@gmail.com>
2023-10-04 09:20:06 -0700

    argc

--- 0x0A-argc_argv/_putchar.c
+++ 0x0A-argc_argv/_putchar.c
@@ -3,10 +3,10 @@
 
 /**
  * _putchar - writes the character c to stdout
- * @c: the character to print
+ * @c: Ther character to print
  *
- * Return: on success 1
- * on error -1 is returned and error is set appropriately
+ * Return: On success 1
+ * on error, -1 is returned and errno is set appropriately
  */
 int _putchar(char c)
 {
