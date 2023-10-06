085375e626650145b3d4f4710be112758c1f7030
Onyeodili Onyekwere Isdore <onyeodiliisdore@gmail.com>
2023-10-06 05:12:37 -0700

    main

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,12 +5,11 @@
  * @c: is the char to be checked
  * Return: 1 if char is lowercase, otherwise 0
  */
-
 int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
 	{
 		return (1);
 	}
-		return (0);
+	return (0);
 }
