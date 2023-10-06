ec658245442e0dfd1c7e02dc712491737b67b8b8
Onyeodili Onyekwere Isdore <onyeodiliisdore@gmail.com>
2023-10-06 08:14:23 -0700

    hacker

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - Checks for a digit (0 through 9)
+ * @c: The digits to be checked
+ * Return: 1 if c is a digit or 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
