29e20289d9c4e65b4d083757a9a0a7f3734b91bb
Onyeodili Onyekwere Isdore <onyeodiliisdore@gmail.com>
2023-10-10 18:01:49 -0700

    preprocessor

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-
 /**
- * main - print the name of the file
- *
- * Return: Always 0 (Success)
+ * main - print the name of the file was compiled from
+ * Return: Always 0
  */
+
 int main(void)
 {
-	printf("%d\n", FILE);
+	printf("%s\n", FILE);
+
 	return (0);
 }
